191063df67c3178563d40cce0831745a2f49b6e7
dth26 <danielhui123@gmail.com>
2018-01-02 22:22:07 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,13 +1,4 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
